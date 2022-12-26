26102b6a28bf6369d1bdf70ad5df63ac33b9b542
arkanzafran <91584760+arkanzafran@users.noreply.github.com>
2022-12-26 11:13:36 +0700

    Delete MODUL1_ARKAN directory

--- MODUL1_ARKAN/src/public class Menu {.java
+++
@@ -1,6 +0,0 @@
-public class Menu {
-    public String Name;
-    system.out.println("1. Bakso")
-    public String Category;
-    public int Price;
-}
--- MODUL1_ARKAN/src/public class User{.java
+++
@@ -1,6 +0,0 @@
-public class User{
-    public String Nama_pendek_kelas;
-    system.out.println("Name : Druce Arkan Zafran_SI4505")
-    public String No_handphone;
-    system.out.println("NO HP : 0811718092")
-}
