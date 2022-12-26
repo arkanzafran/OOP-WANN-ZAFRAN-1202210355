ce67e19abb6e1042527b5113284d7c55169024b7
arkanzafran <91584760+arkanzafran@users.noreply.github.com>
2022-12-26 11:11:02 +0700

    Add files via upload

---
+++ MODUL1_ARKAN/src/public class Main {Menu.java
@@ -0,0 +1,20 @@
+public class Main {Menu
+string name;
+string category;
+string price;
+
+public string void getName(){
+    return name;
+}
+
+public string void getCategory(){
+    return category;
+}
+
+public string void getPrice(){
+    return price;
+}
+
+
+
+}
